d40c5bd5a4e09d60e2bfc945ddb3acb882b4efa2
Rany <ranyel@msn.com>
2020-03-28 08:15:09 -0700

    add Props and State

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>By Rany</p>
-    </div>
-  );
+interface Props {}
+interface State {}
+
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    console.log('[App.tsx] -- constructor', props, this.state);
+  }
+  render() {
+    // write jscript here
+    console.log('[App.tsx] -- render', this.props, this.state);
+    return (
+      <div className="App">
+        <p>By Rany</p>
+      </div>
+    );
+  }
 }
 
 export default App;
